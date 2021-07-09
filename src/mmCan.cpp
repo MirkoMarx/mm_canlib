a9dfe499d14a61ebe275347f5e130b5c14fbd92b
mirko <kqxtvqpr@gmail.com>
2021-07-09 17:35:09 +0200

    Updated

--- src/mmCan.cpp
+++ src/mmCan.cpp
@@ -1,5 +1,21 @@
 #include "mmCan.h"
 
+mm::receive_filter::receive_filter(const uint32_t& min_can_id, const uint32_t& max_can_id)
+    : m_min_can_id(min_can_id)
+    , m_max_can_id(max_can_id)
+{
+}
+
+uint32_t mm::receive_filter::min_can_id()
+{
+    return m_min_can_id;
+}
+
+uint32_t mm::receive_filter::max_can_id()
+{
+    return m_max_can_id;
+}
+
 mm::can::can(const std::string& interface_name)
 {
 
--- src/mmCan.h
+++ src/mmCan.h
@@ -5,15 +5,36 @@
 
 namespace mm {
 
+class receive_filter
+{
+public:
+    receive_filter(const uint32_t& min_can_id, const uint32_t& max_can_id);
+    uint32_t min_can_id();
+    uint32_t max_can_id();
+private:
+    uint32_t m_min_can_id;
+    uint32_t m_max_can_id;
+};
+
 class can
 {
 public:
     can(const std::string& interface_name);
 
+    void init();
+    void deinit();
+
     template<class Arg, class F>
     void set_receive_callback(Arg&& arg, F&& f);
 
     void error_counters(uint8_t& rx, uint8_t& tx);
+
+    void add_receive_filter(const receive_filter& filter);
+
+    void transmit_can_message(const uint32_t& can_id, const uint8_t& frame_type, const uint8_t& dlc, const uint8_t* data);
+
+    static void availiable_interface_names();
+    static std::string version();
 
 private:
     std::function<void(uint32_t, uint8_t, uint8_t, const uint8_t*)> m_receive_callback;
